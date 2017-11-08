cc02f5fc60609f13d76a3f2d078403c533f882d1
gogotanaka <mail@tanakakazuki.com>
2017-11-08 21:47:41 +0900

    Show io

--- src/app_container.js
+++ src/app_container.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import axios from 'axios';
 export default class AppContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ip: null
+    }
+  }
+
+  componentWillMount() {
+    axios.get('https://api.ipify.org?format=json')
+    .then((response) => {
+      this.setState({ ip: response.data.ip })
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
   render() {
     return(
-      <div>
-        test
-      </div>
+      <div>{this.state.ip}</div>
     );
   }
 }
